d60521b6dae0db0c887a24547b39116d3d2890ce
JMCM17 <108616519+JMCM17@users.noreply.github.com>
2023-03-06 13:08:46 +0800

    Add files via upload

--- BatStateU Portals/main.py
+++ BatStateU Portals/main.py
@@ -1,26 +1,15 @@
 from flask import Flask, render_template, request
-from bg import backgrounds
 import random
-import random_lists
+import unorganized
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    background_url = random.choice(backgrounds)
-    return render_template('login-base.html', background=background_url, campuses=random_lists.campuses)
-
-
-@app.route('/handle_form', methods=['POST'])
-def handle_form():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    remember_me = request.form.get('remember_me')
-    # Do something with the form data
-    print(username, password)
-    return 'Form submitted successfully!'
+    background = random.choice(unorganized.backgrounds)
+    return render_template('login.html', background_url=background, campuses=unorganized.campuses)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ BatStateU Portals/static/app.js
@@ -0,0 +1,35 @@
+const showHiddenPass = (inputPass, inputIcon) => {
+    const input = document.getElementById(inputPass),
+        iconEye = document.getElementById(inputIcon)
+
+    iconEye.addEventListener('click', () => {
+        // Change password to text
+        if (input.type == 'password') {
+            // Switch to text
+            input.type = 'text'
+
+            // Add icon
+            iconEye.classList.add('ri-eye-line')
+
+            // Remove icon
+            iconEye.classList.remove('ri-eye-off-line')
+        } else {
+            // Change to password
+            input.type = 'password'
+
+            // Remove icon
+            iconEye.classList.remove('ri-eye-line')
+
+            // Add icon
+            iconEye.classList.add('ri-eye-off-line')
+        }
+    })
+}
+
+function getSelectValue() {
+    const selectedValue = document.getElementById('campus-select').value;
+    let emailInput = document.getElementById('read-only-email');
+    emailInput.value = selectedValue;
+}
+
+showHiddenPass('input-pass', 'input-icon')
